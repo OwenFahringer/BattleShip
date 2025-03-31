b3429554a8bfed7d9f05abbc4ac184dd8a046d0d
OwenFahringer <owenfahringer@gmail.com>
2025-03-31 13:40:25 -0400

    Initial Commit

---
+++ BattleShip/BattleShipApp.swift
@@ -0,0 +1,17 @@
+//
+//  BattleShipApp.swift
+//  BattleShip
+//
+//  Created by Owen F on 3/31/25.
+//
+
+import SwiftUI
+
+@main
+struct BattleShipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BattleShipTests/BattleShipTests.swift
@@ -0,0 +1,17 @@
+//
+//  BattleShipTests.swift
+//  BattleShipTests
+//
+//  Created by Owen F on 3/31/25.
+//
+
+import Testing
+@testable import BattleShip
+
+struct BattleShipTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
